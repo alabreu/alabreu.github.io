38102a2ab58262ea41ca8fdcb07cb258a3e5bc33
alabreu <alexandre.oliveira.abreu@gmail.com>
2026-03-26 14:31:14 -0300

    fix: use HashRouter for GitHub Pages static hosting compatibility

--- mtg-deck-builder/src/App.tsx
+++ mtg-deck-builder/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DeckBuilder from './pages/DeckBuilder';
 import DesignSystem from './pages/DesignSystem';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/deck/:id" element={<DeckBuilder />} />
         <Route path="/design" element={<DesignSystem />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
